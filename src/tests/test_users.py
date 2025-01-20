3063b45119a4952514c46a65454fde619ab4be24
zabit <xaclafun1991@gmail.com>
2025-01-20 23:01:53 +0300

    add tests and readme

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -24,10 +24,25 @@ async def test_register_user(test_client: AsyncClient):
 
 
 @pytest.mark.asyncio
+async def test_activate_user(test_client: AsyncClient, session: AsyncSession):
+    UserFactory._meta.sqlalchemy_session = session
+    user = UserFactory(is_active=False)
+    session.add(user)
+    await session.commit()
+
+    response = await test_client.get(f"/users/activate/{user.id}")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["message"] == "User successfully activated."
+
+
+@pytest.mark.asyncio
 async def test_login_user(test_client: AsyncClient, session: AsyncSession):
     UserFactory._meta.sqlalchemy_session = session
     user = UserFactory(
-        username="testlogin", email="testlogin@example.com", is_active=True
+        username="testlogin",
+        email="testlogin@example.com",
+        is_active=True,
     )
     session.add(user)
     await session.commit()
@@ -43,35 +58,93 @@ async def test_login_user(test_client: AsyncClient, session: AsyncSession):
 
 
 @pytest.mark.asyncio
-async def test_activate_user(test_client: AsyncClient, session: AsyncSession):
+async def test_update_user(test_client: AsyncClient, session: AsyncSession):
     UserFactory._meta.sqlalchemy_session = session
-    user = UserFactory(is_active=False)
+    user = UserFactory(username="updatableuser", is_active=True)
     session.add(user)
     await session.commit()
 
-    response = await test_client.get(f"/users/activate/{user.id}")
+    response = await test_client.patch(
+        "/users",
+        data={"first_name": "UpdatedName"},
+    )
     assert response.status_code == 200
     data = response.json()
-    assert data["message"] == "User successfully activated."
+    assert data["first_name"] == "UpdatedName"
 
 
-# @pytest.mark.asyncio
-# async def test_get_me(test_client: AsyncClient, session: AsyncSession):
-#     UserFactory._meta.sqlalchemy_session = session
-#     user = UserFactory(username="testgetme", email="testgetme@example.com")
-#     session.add(user)
-#     await session.commit()
-#
-#     login_response = await test_client.post(
-#         "/users/login",
-#         json={"username": "testgetme", "password": "password123"},
-#     )
-#     access_token = login_response.json()["access_token"]
-#
-#     response = await test_client.get(
-#         "/users/get-me", headers={"Authorization": f"Bearer {access_token}"}
-#     )
-#     assert response.status_code == 200
-#     data = response.json()
-#     assert data["username"] == "testgetme"
-#     assert data["email"] == "testgetme@example.com"
+@pytest.mark.asyncio
+async def test_get_me(test_client: AsyncClient, session: AsyncSession):
+    UserFactory._meta.sqlalchemy_session = session
+    user = UserFactory(username="meuser", is_active=True)
+    session.add(user)
+    await session.commit()
+
+    response = await test_client.get(
+        "/users/get-me",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["username"] == "meuser"
+
+
+@pytest.mark.asyncio
+async def test_get_all_users(test_client: AsyncClient, session: AsyncSession):
+    UserFactory._meta.sqlalchemy_session = session
+    superuser = UserFactory(is_superuser=True)
+    session.add(superuser)
+    await session.commit()
+
+    response = await test_client.get(
+        "/users",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+
+
+@pytest.mark.asyncio
+async def test_get_user_by_id(test_client: AsyncClient, session: AsyncSession):
+    UserFactory._meta.sqlalchemy_session = session
+    user = UserFactory(username="singleuser", is_active=True)
+    session.add(user)
+    await session.commit()
+
+    response = await test_client.get(
+        f"/users/{user.id}",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert data["username"] == "singleuser"
+
+
+@pytest.mark.asyncio
+async def test_change_online_status(test_client: AsyncClient, session: AsyncSession):
+    UserFactory._meta.sqlalchemy_session = session
+    user = UserFactory(is_online=False, is_active=True)
+    session.add(user)
+    await session.commit()
+
+    response = await test_client.get(
+        "/users/change-online-status",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert "User status has changed to: {user.is_online}." in data["message"]
+
+
+@pytest.mark.asyncio
+async def test_change_ignore_status(test_client: AsyncClient, session: AsyncSession):
+    UserFactory._meta.sqlalchemy_session = session
+    user = UserFactory(ignore_messages=False, is_active=True)
+    session.add(user)
+    await session.commit()
+
+    response = await test_client.get(
+        "/users/change-ignore-status",
+    )
+    assert response.status_code == 200
+    data = response.json()
+    assert (
+        f"User ignore status has changed to: {user.ignore_messages}." in data["message"]
+    )
