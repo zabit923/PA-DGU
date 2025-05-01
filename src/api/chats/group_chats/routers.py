80d30ae226edafadfde9c94f3076d71a651040a5
zabit <xaclafun1991@gmail.com>
2025-05-01 13:31:14 +0300

    fix websockets

--- src/api/chats/group_chats/routers.py
+++ src/api/chats/group_chats/routers.py
@@ -1,9 +1,13 @@
 import logging
+from json import JSONDecodeError
 from typing import List
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, WebSocketException, status
+from starlette.websockets import WebSocket, WebSocketDisconnect
 
+from api.chats.dependencies import authorize_websocket
 from api.groups.service import GroupService, group_service_factory
+from api.notifications.service import NotificationService, notification_service_factory
 from api.users.routers import get_current_user
 from core.database.models import User
 from core.managers.group_websocket_manager import (
@@ -11,11 +15,69 @@ from core.managers.group_websocket_manager import (
     get_group_websocket_manager,
 )
 
-from .schemas import GroupMessageRead, GroupMessageUpdate
+from .schemas import GroupMessageCreate, GroupMessageRead, GroupMessageUpdate
 from .service import GroupChatService, group_chat_service_factory
 
 router = APIRouter(prefix="/groups")
 logger = logging.getLogger(__name__)
+
+
+@router.websocket("/{group_id}")
+async def group_chat_websocket(
+    group_id: int,
+    websocket: WebSocket,
+    user: User = Depends(authorize_websocket),
+    chat_service: GroupChatService = Depends(group_chat_service_factory),
+    group_service: GroupService = Depends(group_service_factory),
+    notification_service: NotificationService = Depends(notification_service_factory),
+    manager: GroupConnectionManager = Depends(get_group_websocket_manager),
+):
+    group = await group_service.get_group(group_id)
+    if not group:
+        raise WebSocketException(
+            code=status.WS_1008_POLICY_VIOLATION, reason="Group not found."
+        )
+    if user not in group.members:
+        raise WebSocketException(
+            code=status.WS_1008_POLICY_VIOLATION,
+            reason="User not member of this group.",
+        )
+    await manager.connect(group_id, user.username, websocket)
+    await chat_service.update_online_status(user)
+    try:
+        while True:
+            try:
+                message_data = await websocket.receive_json()
+                if "action" in message_data and message_data["action"] == "typing":
+                    is_typing = message_data.get("is_typing", False)
+                    await manager.notify_typing_status(
+                        group_id, user.username, is_typing
+                    )
+                    continue
+                message_data = GroupMessageCreate(**message_data)
+                message = await chat_service.create_message(
+                    message_data, user, group_id
+                )
+                await notification_service.create_group_message_notification(
+                    message, chat_service
+                )
+                message = GroupMessageRead.model_validate(message).model_dump(
+                    mode="json"
+                )
+                await manager.broadcast(group_id, message, user.username)
+            except (JSONDecodeError, AttributeError) as e:
+                logger.exception(f"Websocket error, detail: {e}")
+                await manager.send_error("Wrong message format", websocket)
+                continue
+            except ValueError as e:
+                logger.exception(f"Websocket error, detail: {e}")
+                await manager.send_error(
+                    "Could not validate incoming message", websocket
+                )
+    except WebSocketDisconnect:
+        await chat_service.update_online_status(user)
+        logging.info("Websocket is disconnected")
+        manager.disconnect(group_id, user.username)
 
 
 @router.get(
--- src/api/chats/private_chats/routers.py
+++ src/api/chats/private_chats/routers.py
@@ -1,10 +1,14 @@
 import logging
+from json import JSONDecodeError
 from typing import List
 
 from fastapi import APIRouter
 from fastapi.params import Depends
 from starlette import status
+from starlette.websockets import WebSocket, WebSocketDisconnect
 
+from api.chats.dependencies import authorize_websocket
+from api.notifications.service import NotificationService, notification_service_factory
 from api.users.dependencies import get_current_user
 from core.database.models import User
 from core.managers.private_websocket_manager import (
@@ -12,11 +16,63 @@ from core.managers.private_websocket_manager import (
     get_private_websocket_manager,
 )
 
-from .schemas import PrivateMessageRead, PrivateMessageUpdate, RoomRead
+from .schemas import (
+    PrivateMessageCreate,
+    PrivateMessageRead,
+    PrivateMessageUpdate,
+    RoomRead,
+)
 from .service import PrivateChatService, private_chat_service_factory
 
 router = APIRouter(prefix="/private-chats")
 logger = logging.getLogger(__name__)
+
+
+@router.websocket("/{receiver_id}")
+async def private_chat_websocket(
+    receiver_id: int,
+    websocket: WebSocket,
+    user: User = Depends(authorize_websocket),
+    chat_service: PrivateChatService = Depends(private_chat_service_factory),
+    notification_service: NotificationService = Depends(notification_service_factory),
+    manager: PrivateConnectionManager = Depends(get_private_websocket_manager),
+):
+    room = await chat_service.get_or_create_room(user_id1=user.id, user_id2=receiver_id)
+    await manager.connect(room.id, user.username, websocket)
+    await chat_service.update_online_status(user)
+    try:
+        while True:
+            try:
+                message_data = await websocket.receive_json()
+                if "action" in message_data and message_data["action"] == "typing":
+                    is_typing = message_data.get("is_typing", False)
+                    await manager.notify_typing_status(
+                        room.id, user.username, is_typing
+                    )
+                    continue
+                message_data = PrivateMessageCreate(**message_data)
+                message = await chat_service.create_message(user, room.id, message_data)
+                await notification_service.create_private_message_notification(
+                    message, chat_service
+                )
+                message = PrivateMessageRead.model_validate(message).model_dump(
+                    mode="json"
+                )
+                await manager.send_message(room.id, message, exclude=user.username)
+
+            except (JSONDecodeError, AttributeError) as e:
+                logger.exception(f"Websocket error, detail: {e}")
+                await manager.send_error("Wrong message format", websocket)
+                continue
+            except ValueError as e:
+                logger.exception(f"Websocket error, detail: {e}")
+                await manager.send_error(
+                    "Could not validate incoming message", websocket
+                )
+    except WebSocketDisconnect:
+        await chat_service.update_online_status(user)
+        logger.info("Websocket is disconnected")
+        manager.disconnect(room.id, user.username)
 
 
 @router.get(
